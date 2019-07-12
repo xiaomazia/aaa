40f019ebc5ce093e7d3c31f0759acfea2260c74a
xiaomazia <45998821+xiaomazia@users.noreply.github.com>
2019-07-12 15:45:38 +0800

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,9 @@
+<?php
+  namespace api\controller;
+  use thinkphp\controller;
+  class Index extends controller{
+      public function __construct(App $app = null)
+    {
+        parent::__construct($app);
+    }
+  }
